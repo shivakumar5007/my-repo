84692b0ce202323b03bd7515fe1a00aac450ef4a
Chanugomula Shiva Kumar <104490951+shivakumar5007@users.noreply.github.com>
2022-10-17 06:43:02 +0530

    Delete MyAnnotation.java

--- MyAnnotation.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.*;
-@Target(ElementType.TYPE)
-@Inherited
-@Retention(RetentionPolicy.RUNTIME)
-@interface MyAnnotation
-{
-	 String author() default "Ramesh Ponnala";
-	 String course() default "OOP Java";
-}
